05f3e38ea950e74597048651d8c532d9c30d7fbd
Tran Minh Quan Nguyen <90527518+minhquan050903@users.noreply.github.com>
2023-05-29 19:56:24 -0400

    Added comments to testing files

--- testing.py
+++ testing.py
@@ -1,8 +1,9 @@
-import sqlite3
-import pandas 
-conn = sqlite3.connect('database.sqlite')
+import sqlite3 #imprort sqlite3 library to import sql3 file
+import pandas #import pandas library for sql queries
+import numpy as np #import numpy library
+conn = sqlite3.connect('database.sqlite') #Connact to soccer database
 
 tables = pandas.read_sql("""SELECT *
                         FROM sqlite_master
-                        WHERE type='table';""", conn)
+                        WHERE type='table';""", conn) #running tested queries
 tables
